9a97bd1bf34b22c8207102de9d955554dfd431d4
Zhugalev Sergei <zhugalcf@gmail.com>
2023-06-04 11:56:00 +0300

    Create classes of Heroes

---
+++ src/main/java/ru/faang/school/task_1/Creature.java
@@ -0,0 +1,13 @@
+package ru.faang.school.task_1;
+
+public abstract class Creature {
+    private String name;
+    private int level;
+    private int force;
+    private int defence;
+    private int speed;
+    private int quantity;
+
+    public abstract int getDamage();
+
+}
