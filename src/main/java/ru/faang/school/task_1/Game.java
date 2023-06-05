3a739b00c11c189ee5fed2f5034f66dcacdd7bd5
Zhugalev Sergei <zhugalcf@gmail.com>
2023-06-05 15:54:25 +0300

    Revert "test"
    This reverts commit 44c642b5cb892a0e552c65473a6c4aa8f303fd0b.

--- src/main/java/ru/faang/school/task_1/Game.java
+++ src/main/java/ru/faang/school/task_1/Game.java
@@ -12,6 +12,5 @@ public class Game {
         battlefield.getHero2().removeCreature(Swordman.class,5);
 
         System.out.println("Winner is: " + battlefield.battle().toString());
-        System.out.println((double) (400 / 1700));
     }
 }
